b19556a20da6641c7d0d8509ac04c186dbcd471b
Michael Korolkov <83701771+fransisdual@users.noreply.github.com>
2021-06-16 13:39:07 +0300

    Добавьте файлы проекта.

---
+++ BillionFileDownloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BillionFileDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
